094174714867ea35d479e54c3b9e0282ce1787bb
charleslukes <charleslukes28@gmail.com>
2019-12-29 19:59:36 +0100

    add resolvers to the user fields

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,9 +1,39 @@
+const {
+  createUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser
+} = require("../db/user");
+
 module.exports.userResolver = {
   Query: {
+    getUser: (_, args) => {
+      const { id } = args;
+      return getUser(id);
+    },
     getUsers: () => {
-      return [
-        { id: 3, firstname: "Charles", lastname: "Chiakwa", country: "Nigeria" }
-      ];
+      return getAllUsers();
+    }
+  },
+
+  Mutation: {
+    createUser: (_, args) => {
+      const { firstname, lastname, email, country } = args;
+      const data = createUser(firstname, lastname, email, country);
+
+      return data;
+    },
+
+    updateUser: (_, args) => {
+      const { firstname, lastname, email, country, id } = args;
+      return updateUser(firstname, lastname, email, country, id);
+    },
+
+    deleteUser: (_, args) => {
+      const { id } = args;
+
+      return deleteUser(id);
     }
   }
 };
